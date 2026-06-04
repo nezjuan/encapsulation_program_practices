e7b12f623141b5e3a0b4541cfa7bf3b5ff4a25b6
nezjuan <juanmartinezat9@gmail.com>
2026-06-04 17:08:35 +0800

    created a setters file for access modifier

---
+++ fan_class_program/setters_file.py
@@ -0,0 +1,12 @@
+class Setters():
+    def set_speed(self, speed):
+        self.__speed= speed
+
+    def set_radius(self, radius):
+        self.__radius= radius
+   
+    def set_color(self, color):
+        self.__color= color
+  
+    def get_on_status(self, on_status):
+        self.__on_status= on_status
