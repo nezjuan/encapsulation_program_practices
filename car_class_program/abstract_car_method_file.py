5461c8dac051de06837ffcf114d489d469f71ebb
nezjuan <juanmartinezat9@gmail.com>
2026-06-04 19:05:54 +0800

    created abstract file for the second program

---
+++ car_class_program/abstract_car_method_file.py
@@ -0,0 +1,14 @@
+from abc import ABC,abstractmethod
+
+class Car(ABC):
+    def __init__(self, year_model, make, speed):
+        pass
+
+    def accelerate():
+        pass
+
+    def brake():
+        pass
+    
+    def get_speed():
+        pass
