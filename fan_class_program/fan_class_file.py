74fbee5735aaa1dec13972b92e1e0bb947d29a0f
nezjuan <juanmartinezat9@gmail.com>
2026-06-04 16:57:35 +0800

    created a getters file to be inherited by the main fan class file so it would look cleaner i guess

--- fan_class_program/fan_class_file.py
+++ fan_class_program/fan_class_file.py
@@ -1,11 +1,10 @@
 from abstract_fan_class_file import Fan
+from getters_file import Getters
 
-class FanFile(Fan):
+class FanFile(Fan, Getters):
 
     def __init__(self, speed, radius, color, on_status=False):
         self.__speed= speed
         self.__radius= radius
         self.__color= color
         self.__on_status= on_status
-
-    
---
+++ fan_class_program/getters_file.py
@@ -0,0 +1,12 @@
+class Getters():
+    def get_speed(self):
+        return self.__speed
+
+    def get_radius(self):
+        return self.__radius
+
+    def get_color(self):
+        return self.__color
+
+    def get_on_status(self):
+        return self.__on_status
